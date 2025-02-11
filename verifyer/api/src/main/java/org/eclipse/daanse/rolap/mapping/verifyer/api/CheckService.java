5ccca33690279d548113e0d81740664d5ef4b342
dbulahov <bulahovdenis@gmail.com>
2025-02-11 17:27:04 +0300

    migrate db check service from rdb repository Signed-off-by: dbulahov <bulahovdenis@gmail.com>

---
+++ verifyer/api/src/main/java/org/eclipse/daanse/rolap/mapping/verifyer/api/CheckService.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   SmartCity Jena - initial
+ *   Stefan Bischof (bipolis.org) - initial
+ */
+package org.eclipse.daanse.rolap.mapping.verifyer.api;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.eclipse.daanse.rolap.mapping.api.model.CatalogMapping;
+
+
+
+public interface CheckService {
+
+    List<VerificationResult> verify(CatalogMapping catalog, DataSource dataSource);
+}
