fb589753a4696c284a7fac3756784e0166c05ea8
Stefan Bischof <stbischof@bipolis.org>
2025-03-15 16:48:43 +0100

    Add initial implementation for dimension introduction tutorial
    - Add `pom.xml` for Maven project setup
    - Implement `CatalogSupplier` class for catalog mapping
    - Create `Fact.csv` with sample data
    - Define database schema and cube structure
    - Document various components of the cube
    - Configure OSGi component for `CatalogMappingSupplier`
    
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ instance/emf/tutorial/cube/dimension.intro/src/main/java/org/eclipse/daanse/rolap/mapping/instance/emf/tutorial/cube/dimension/intro/CatalogSupplier.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *
+ */
+package org.eclipse.daanse.rolap.mapping.instance.emf.tutorial.cube.dimension.intro;
+
+import static org.eclipse.daanse.rolap.mapping.emf.rolapmapping.provider.util.DocumentationUtil.document;
+
+import java.util.List;
+
+import org.eclipse.daanse.rolap.mapping.api.CatalogMappingSupplier;
+import org.eclipse.daanse.rolap.mapping.api.model.CatalogMapping;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.Catalog;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.Column;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.ColumnType;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.DatabaseSchema;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.DimensionConnector;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.Hierarchy;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.Level;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.Measure;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.MeasureAggregator;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.MeasureGroup;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.PhysicalCube;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.PhysicalTable;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.RolapMappingFactory;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.StandardDimension;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.TableQuery;
+import org.osgi.service.component.annotations.Component;
+
+@Component(service = CatalogMappingSupplier.class)
+public class CatalogSupplier implements CatalogMappingSupplier {
+
+    private static final String introBody = """
+            beside the Measures a cube can contain dimensions. Dimensions are used to slice, dice and piveaut the data in the cube. They can be used group the data, filter the data,or to provide additional context for the data. Dimensions consist of one or multiple Hierarchies. Hierarchies consist of one or multiple Levels. Each level can have one or multiple Properties. Properties are used to provide additional context for the data. They can be used to display additional information about the data in the cube, such as descriptions, labels, or other attributes.
+
+            In this example we will create the most simple version, a cube with one dimension. The dimension will have one hierarchy, which will have one level.
+            """;
+
+    private static final String databaseSchemaBody = """
+            The cube defined in this example is based on a single table that stores all the data. The table, named `Fact`, contains two columns: `KEY` and `VALUE`. The `KEY` column acts as a discriminator, while the `VALUE` column holds the measurements to be aggregated.
+            """;
+
+    private static final String queryBody = """
+            This example uses a TableQuery, as it directly references the physical table `Fact`.
+            """;
+
+    private static final String levelBody = """
+            The level is the most basic element of a hierarchy. It defines the granularity of the data in the hierarchy. To create a Level we need do set the key column, which is the column that uniquely identifies the Level in the table. In this example we use the `KEY` column of the `Fact` table as the key column of the level.
+            """;
+
+    private static final String hierarchyBody = """
+            A hierarchy is a collection of levels that defines the structure of data within a dimension. It provides a name for the group the contained Level elements.
+
+            To define a hierarchy, a query is required. This query is used to retrieve the data for the hierarchy. In this example, we use the same query as for the cube, but this is only for simplicity.
+
+            Additionally, a primary key column can be specified for the hierarchy. The primary key column uniquely identifies members and is referenced by rows in the fact table. However, setting a primary key column is not mandatory. If it is not defined, the server will automatically use the key of the lowest level in the hierarchy.
+            """;
+
+    private static final String dimensionBody = """
+            The Dimenaion is the main element of the cube. It defines the structure of the data in the cube. The dimension can contain one or multiple hierarchies. In this example we use one hierarchy, which contains one level.
+            """;
+
+    private static final String cubeBody = """
+            The cube contains only one Measure in a unnamed MeasureGroup and references to the Dimension.
+
+            To connect the dimension to the cube, a DimensionConnector is used. The DimensionConnector defines the connection between the cube and the dimension. It can also be used to override the name of the dimension in the cube, this is relevant if you want to use the same dimension multiple times in one cube. But one Dimension also could be used in multiple cubes.
+            """;
+
+    @Override
+    public CatalogMapping get() {
+        DatabaseSchema databaseSchema = RolapMappingFactory.eINSTANCE.createDatabaseSchema();
+        databaseSchema.setId("_dbschema");
+
+        Column keyColumn = RolapMappingFactory.eINSTANCE.createPhysicalColumn();
+        keyColumn.setName("KEY");
+        keyColumn.setId("_col_key");
+        keyColumn.setType(ColumnType.VARCHAR);
+
+        Column valueColumn = RolapMappingFactory.eINSTANCE.createPhysicalColumn();
+        valueColumn.setName("VALUE");
+        valueColumn.setId("_col_value");
+        valueColumn.setType(ColumnType.INTEGER);
+
+        PhysicalTable table = RolapMappingFactory.eINSTANCE.createPhysicalTable();
+        table.setName("Fact");
+        table.setId("_tab");
+        table.getColumns().addAll(List.of(keyColumn, valueColumn));
+        databaseSchema.getTables().add(table);
+
+        TableQuery query = RolapMappingFactory.eINSTANCE.createTableQuery();
+        query.setId("_query");
+        query.setTable(table);
+
+        Measure measure = RolapMappingFactory.eINSTANCE.createMeasure();
+        measure.setAggregator(MeasureAggregator.SUM);
+        measure.setName("theMeasure");
+        measure.setId("_measure");
+        measure.setColumn(valueColumn);
+
+        MeasureGroup measureGroup = RolapMappingFactory.eINSTANCE.createMeasureGroup();
+        measureGroup.getMeasures().add(measure);
+
+        Level level = RolapMappingFactory.eINSTANCE.createLevel();
+        level.setName("theLevel");
+        level.setId("_level");
+        level.setColumn(keyColumn);
+
+        Hierarchy hierarchy = RolapMappingFactory.eINSTANCE.createHierarchy();
+        hierarchy.setName("theHierarchy");
+        hierarchy.setId("_hierarchy");
+        hierarchy.setPrimaryKey(keyColumn);
+        hierarchy.setQuery(query);
+        hierarchy.getLevels().add(level);
+
+        StandardDimension dimension = RolapMappingFactory.eINSTANCE.createStandardDimension();
+        dimension.setName("theDimension");
+        dimension.setId("_dimension");
+        dimension.getHierarchies().add(hierarchy);
+
+        DimensionConnector dimensionConnector = RolapMappingFactory.eINSTANCE.createDimensionConnector();
+//        dimensionConnector.setOverrideDimensionName("Dimension");
+        dimensionConnector.setDimension(dimension);
+
+        PhysicalCube cube = RolapMappingFactory.eINSTANCE.createPhysicalCube();
+        cube.setName("CubeWithSimpleDimension");
+        cube.setId("_cube");
+        cube.setQuery(query);
+        cube.getMeasureGroups().add(measureGroup);
+        cube.getDimensionConnectors().add(dimensionConnector);
+
+        Catalog catalog = RolapMappingFactory.eINSTANCE.createCatalog();
+        catalog.setName("Dimension - Introduction");
+        catalog.setId("_catalog");
+        catalog.getCubes().add(cube);
+
+        document(catalog, "Dimension - Introduction", introBody, 1, 0, 0, false, 0);
+        document(databaseSchema, "Database Schema", databaseSchemaBody, 1, 1, 0, true, 3);
+        document(query, "Query", queryBody, 1, 2, 0, true, 2);
+
+        document(level, "Level", levelBody, 1, 3, 0, true, 2);
+        document(hierarchy, "Hierarchy", hierarchyBody, 1, 4, 0, true, 2);
+        document(dimension, "Dimension", dimensionBody, 1, 5, 0, true, 2);
+
+        document(cube, "Cube and DimensionConnector and Measure", cubeBody, 1, 6, 0, true, 2);
+
+        return catalog;
+    }
+
+}
