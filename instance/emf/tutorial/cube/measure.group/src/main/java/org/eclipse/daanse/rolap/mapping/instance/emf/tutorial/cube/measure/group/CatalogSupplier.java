484752cb35e54b06aa3ebb64a1200aa3c543a3ab
Stefan Bischof <stbischof@bipolis.org>
2025-03-15 15:22:34 +0100

    Add measure group support with CatalogSupplier and sample data
    - Add `pom.xml` for measure group module
    - Implement `CatalogSupplier` for measure group
    - Define `DatabaseSchema`, `PhysicalTable`, and `TableQuery`
    - Create `MeasureGroup` and `PhysicalCube` examples
    - Document the catalog and its components
    - Add sample data in `Fact.csv`
    
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ instance/emf/tutorial/cube/measure.group/src/main/java/org/eclipse/daanse/rolap/mapping/instance/emf/tutorial/cube/measure/group/CatalogSupplier.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *
+ */
+package org.eclipse.daanse.rolap.mapping.instance.emf.tutorial.cube.measure.group;
+
+import static org.eclipse.daanse.rolap.mapping.emf.rolapmapping.provider.util.DocumentationUtil.document;
+
+import java.util.List;
+
+import org.eclipse.daanse.rolap.mapping.api.CatalogMappingSupplier;
+import org.eclipse.daanse.rolap.mapping.api.model.CatalogMapping;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.Catalog;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.Column;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.ColumnType;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.DatabaseSchema;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.Measure;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.MeasureAggregator;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.MeasureGroup;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.PhysicalCube;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.PhysicalTable;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.RolapMappingFactory;
+import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.TableQuery;
+import org.osgi.service.component.annotations.Component;
+
+@Component(service = CatalogMappingSupplier.class)
+public class CatalogSupplier implements CatalogMappingSupplier {
+
+    private static final String introBody = """
+            In cases where you need a better overview of all measures, want to provide additional context for a `Measure`, or need to organize a list of measures, you can use a `MeasureGroup`.
+            """;
+
+    private static final String databaseSchemaBody = """
+            The cube defined in this example is based on a single table that stores all the data. The table, named `Fact`, contains two columns: `KEY` and `VALUE`. The `KEY` column acts as a discriminator, while the `VALUE` column holds the measurements to be aggregated.
+            """;
+
+    private static final String queryBody = """
+            This example uses a TableQuery, as it directly references the physical table `Fact`.
+            """;
+
+    private static final String cubeBody = """
+            A `MeasureGroup` is a logical container designed to group related measures. Each `MeasureGroup` can have a  name. Names must be unique within the `Cube`. `Measure` only can be in `Cube`s if they are contained in `MeasureGroup`s. In this example we group all alphabetic measures in one group and all other measures in another group.
+            """;
+
+    @Override
+    public CatalogMapping get() {
+        DatabaseSchema databaseSchema = RolapMappingFactory.eINSTANCE.createDatabaseSchema();
+        databaseSchema.setId("databaseSchema");
+
+        Column keyColumn = RolapMappingFactory.eINSTANCE.createPhysicalColumn();
+        keyColumn.setName("KEY");
+        keyColumn.setId("_col_key");
+        keyColumn.setType(ColumnType.VARCHAR);
+
+        Column valueColumn = RolapMappingFactory.eINSTANCE.createPhysicalColumn();
+        valueColumn.setName("VALUE");
+        valueColumn.setId("_col");
+        valueColumn.setType(ColumnType.INTEGER);
+
+        PhysicalTable table = RolapMappingFactory.eINSTANCE.createPhysicalTable();
+        table.setName("Fact");
+        table.setId("_tab");
+        table.getColumns().addAll(List.of(keyColumn, valueColumn));
+        databaseSchema.getTables().add(table);
+
+        TableQuery query = RolapMappingFactory.eINSTANCE.createTableQuery();
+        query.setId("_query");
+        query.setTable(table);
+
+        Measure measure1 = RolapMappingFactory.eINSTANCE.createMeasure();
+        measure1.setAggregator(MeasureAggregator.SUM);
+        measure1.setName("Measure A");
+        measure1.setId("_measure1");
+        measure1.setColumn(valueColumn);
+
+        Measure measure2 = RolapMappingFactory.eINSTANCE.createMeasure();
+        measure2.setAggregator(MeasureAggregator.SUM);
+        measure2.setName("Measure B");
+        measure2.setId("_measure2");
+        measure2.setColumn(valueColumn);
+
+        Measure measure3 = RolapMappingFactory.eINSTANCE.createMeasure();
+        measure3.setAggregator(MeasureAggregator.SUM);
+        measure3.setName("Measure 1");
+        measure3.setId("_measure3");
+        measure3.setColumn(valueColumn);
+
+        MeasureGroup measureGroup1 = RolapMappingFactory.eINSTANCE.createMeasureGroup();
+        measureGroup1.setName("Group Alphabetic");
+        measureGroup1.getMeasures().addAll(List.of(measure1, measure2));
+
+        MeasureGroup measureGroup2 = RolapMappingFactory.eINSTANCE.createMeasureGroup();
+        measureGroup2.setName("Group Other");
+        measureGroup2.getMeasures().addAll(List.of(measure3));
+
+        PhysicalCube cube = RolapMappingFactory.eINSTANCE.createPhysicalCube();
+        cube.setName("MeasureGroupCube");
+        cube.setId("_cube");
+        cube.setQuery(query);
+        cube.getMeasureGroups().add(measureGroup1);
+        cube.getMeasureGroups().add(measureGroup2);
+
+        Catalog catalog = RolapMappingFactory.eINSTANCE.createCatalog();
+        catalog.setName("Cube - Measures and Formats");
+        catalog.getCubes().add(cube);
+
+        document(catalog, "Measures Formats", introBody, 1, 0, 0, false, 0);
+        document(databaseSchema, "Database Schema", databaseSchemaBody, 1, 1, 0, true, 3);
+        document(query, "Query", queryBody, 1, 2, 0, true, 2);
+        document(cube, "Grouping Measures", cubeBody, 1, 3, 0, true, 2);
+
+        return catalog;
+
+    }
+
+}
