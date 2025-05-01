4e96470d26588287569e808dc87ef821cd2ba263
Stefan Bischof <stbischof@bipolis.org>
2025-05-01 20:53:54 +0200

    rm dead documentation link
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- instance/emf/serializer/src/test/java/org/eclipse/daanse/rolap/mapping/instance/emf/serializer/ResourceSetWriteReadTest.java
+++ instance/emf/serializer/src/test/java/org/eclipse/daanse/rolap/mapping/instance/emf/serializer/ResourceSetWriteReadTest.java
@@ -322,12 +322,6 @@ public class ResourceSetWriteReadTest {
         sbReadme.append("```");
         sbReadme.append("\n");
 
-        sbReadme.append("## Documentation");
-        sbReadme.append("\n");
-        sbReadme.append("\n");
-        sbReadme.append(
-                "More information and visualisations about this Catalog you can find in the [Documentation](./DOCUMENTATION.MD).");
-        sbReadme.append("\n");
         sbReadme.append("\n");
 
         sbReadme.append("## csv data");
