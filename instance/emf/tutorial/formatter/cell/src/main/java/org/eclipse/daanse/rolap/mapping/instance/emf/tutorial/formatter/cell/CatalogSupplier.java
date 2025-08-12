dfa9f4385b387ba218cd8d4a53d7fcd3e0b78385
dbulahov <bulahovdenis@gmail.com>
2025-08-12 12:49:10 +0300

    add documentation and structure to cell formatter tutorial Signed-off-by: dbulahov <bulahovdenis@gmail.com>

--- instance/emf/tutorial/formatter/cell/src/main/java/org/eclipse/daanse/rolap/mapping/instance/emf/tutorial/formatter/cell/CatalogSupplier.java
+++ instance/emf/tutorial/formatter/cell/src/main/java/org/eclipse/daanse/rolap/mapping/instance/emf/tutorial/formatter/cell/CatalogSupplier.java
@@ -10,7 +10,9 @@
  * Contributors:
  *
  */
-package org.eclipse.daanse.rolap.mapping.instance.emf.tutorial.cellformatter;
+package org.eclipse.daanse.rolap.mapping.instance.emf.tutorial.formatter.cell;
+
+import static org.eclipse.daanse.rolap.mapping.emf.rolapmapping.provider.util.DocumentationUtil.document;
 
 import java.util.List;
 
@@ -28,14 +30,43 @@ import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.PhysicalTable;
 import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.RolapMappingFactory;
 import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.SumMeasure;
 import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.TableQuery;
+import org.eclipse.daanse.rolap.mapping.instance.api.Kind;
+import org.eclipse.daanse.rolap.mapping.instance.api.MappingInstance;
+import org.eclipse.daanse.rolap.mapping.instance.api.Source;
 import org.osgi.service.component.annotations.Component;
 
 @Component(service = CatalogMappingSupplier.class)
+@MappingInstance(kind = Kind.TUTORIAL, number = "2.11.1", source = Source.EMF, group = "Formatter") // NOSONAR
 public class CatalogSupplier implements CatalogMappingSupplier {
 
-    private static final String CATALOG = "tutorial_01-04-04-01_Cube_Measure_CellFormatter";
     private static final String CUBE_NAME = "CubeOneNumericMeasureDifferentDataTypes";
     private static final String FACT = "Fact";
+
+    private static final String catalogBody = """
+            Data cube with measures CellFormatter.
+            """;
+
+    private static final String databaseSchemaBody = """
+            The cube defined in this example is based on a single table that stores all the data. The table, named `Fact`, contains two columns: `KEY` and `VALUE`.
+            The `KEY` column acts as a discriminator, while the `VALUE` column holds the measurements to be aggregated.
+            """;
+
+    private static final String queryBody = """
+            This example uses a TableQuery, as it directly references the physical table `Fact`.
+            """;
+
+    private static final String cellFormatterBody = """
+            Specialized formatter for controlling the presentation of cell values in analytical grids.
+            Cell formatter use reference to class formatter mondrian.rolap.format.CellFormatterImpl implemented CellFormatter interface
+    """;
+
+    private static final String measureBody = """
+            Measure with CellFormatter. measure reference the `VALUE` column and use the following SUM aggregation function.
+    """;
+
+    private static final String cubeBody = """
+            In this example, measure with CellFormatter. measure reference the `VALUE` column and use the following SUM aggregation function.
+            """;
 
     private static final String schemaDocumentationTxt = """
             A minimal cube with CellFormatter with class name
@@ -87,15 +118,21 @@ public class CatalogSupplier implements CatalogMappingSupplier {
         cube.getMeasureGroups().add(measureGroup);
 
         Catalog catalog = RolapMappingFactory.eINSTANCE.createCatalog();
-        catalog.setName("Minimal_Cube_With_Measures_CellFormatter");
+        catalog.setName("Minimal Cube With Measures CellFormatter");
         catalog.setDescription("Schema of a minimal cube with CellFormatter with class name");
         catalog.getCubes().add(cube);
         Documentation schemaDocumentation = RolapMappingFactory.eINSTANCE.createDocumentation();
         schemaDocumentation.setValue(schemaDocumentationTxt);
         catalog.getDocumentations().add(schemaDocumentation);
         catalog.getDbschemas().add(databaseSchema);
-        return catalog;
+        document(catalog, "Minimal Cube With Measures CellFormatter", catalogBody, 1, 0, 0, false, 0);
+        document(databaseSchema, "Database Schema", databaseSchemaBody, 1, 1, 0, true, 3);
+        document(query, "Query", queryBody, 1, 2, 0, true, 2);
+        document(cellFormatter, "CellFormatter", cellFormatterBody, 1, 3, 0, true, 2);
+        document(measure1, "Measure1", measureBody, 1, 4, 0, true, 2);
+        document(cube, "Cube with Measures CellFormatter", cubeBody, 1, 5, 0, true, 2);
 
+        return catalog;
     }
 
 }
