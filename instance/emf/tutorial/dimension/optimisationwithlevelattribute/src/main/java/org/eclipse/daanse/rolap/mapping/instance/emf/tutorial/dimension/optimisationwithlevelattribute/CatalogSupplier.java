3ceca030e58e76e78ac7cf884b82b7b0bf0969f7
dbulahov <bulahovdenis@gmail.com>
2025-08-14 13:11:51 +0300

    add documentation and structure to dimension tutorials Signed-off-by: dbulahov <bulahovdenis@gmail.com>

--- instance/emf/tutorial/dimension/optimisationwithlevelattribute/src/main/java/org/eclipse/daanse/rolap/mapping/instance/emf/tutorial/dimension/optimisationwithlevelattribute/CatalogSupplier.java
+++ instance/emf/tutorial/dimension/optimisationwithlevelattribute/src/main/java/org/eclipse/daanse/rolap/mapping/instance/emf/tutorial/dimension/optimisationwithlevelattribute/CatalogSupplier.java
@@ -10,7 +10,9 @@
  * Contributors:
  *
  */
-package org.eclipse.daanse.rolap.mapping.instance.emf.tutorial.dimensionwithlevelattribute;
+package org.eclipse.daanse.rolap.mapping.instance.emf.tutorial.dimension.optimisationwithlevelattribute;
+
+import static org.eclipse.daanse.rolap.mapping.emf.rolapmapping.provider.util.DocumentationUtil.document;
 
 import java.util.List;
 
@@ -23,7 +25,6 @@ import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.DatabaseSchema;
 import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.DimensionConnector;
 import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.Documentation;
 import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.ExplicitHierarchy;
-import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.Hierarchy;
 import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.JoinQuery;
 import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.JoinedQueryElement;
 import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.Level;
@@ -34,18 +35,82 @@ import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.RolapMappingFactory;
 import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.StandardDimension;
 import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.SumMeasure;
 import org.eclipse.daanse.rolap.mapping.emf.rolapmapping.TableQuery;
+import org.eclipse.daanse.rolap.mapping.instance.api.Kind;
+import org.eclipse.daanse.rolap.mapping.instance.api.MappingInstance;
+import org.eclipse.daanse.rolap.mapping.instance.api.Source;
 import org.osgi.service.component.annotations.Component;
 
 @Component(service = CatalogMappingSupplier.class)
+@MappingInstance(kind = Kind.TUTORIAL, number = "2.9.2", source = Source.EMF, group = "Dimension") // NOSONAR
 public class CatalogSupplier implements CatalogMappingSupplier {
 
-    private static final String CATALOG = "tutorial_Cube_with_CubeDimension_with_level_attribute";
-    private static final String CUBE = "CubeMultipleHierarchy";
+    private static final String CUBE = "Cube";
     private static final String FACT = "Fact";
 
-    private static final String schemaDocumentationTxt = """
-    A basic OLAP schema with DimensionUsage with level attribute
-    Level attribute in DimensionUsage uses for optimize sql inner join
+    private static final String catalogBody = """
+            A basic OLAP schema with two Dimension Connectors with level attribute
+            Level attribute in DimensionConnector uses for optimize sql inner join
+            Level attribute is name of the level to join to
+            If not specified joins to the lowest level of the dimension
+            """;
+
+    private static final String databaseSchemaBody = """
+            The Database Schema contains the Fact table with two columns: DIM_KEY and VALUE.
+            The DATE_KEY column is used as the discriminator in the Hierarchy definitions.
+
+            H1_L1 table with two columns: KEY and NAME
+            HX_L2 table with 3 columns: KEY, NAME, H1L1_KEY, H2L1_KEY
+            """;
+
+    private static final String queryBody = """
+            The Query is a simple TableQuery that selects all columns from the Fact table to use in the measures.
+            """;
+
+    private static final String queryH1L1Body = """
+            The Query is a simple TableQuery that selects all columns from the H1_L1 table to use in the hierarchy join.
+            """;
+
+    private static final String queryHxL2Body = """
+            The Query is a simple TableQuery that selects all columns from the Hx_L2 table to use in the hierarchy join.
+            """;
+
+    private static final String joinQueryBody = """
+            The JoinQuery specifies which TableQueries should be joined. It also defines the columns in each table that are used for the join:
+
+            - In the lower-level table (Hx_L2), the join uses the foreign key H1L1_KEY.
+            - In the upper-level table (H1_L1), the join uses the primary key KEY.
+
+            """;
+
+    private static final String h1Level1Body = """
+            This Example uses H1_Level1 level bases on the KEY column and name column NAME of table H1_L1.
+            """;
+
+    private static final String h1Level2Body = """
+            This Example uses H1_Level2 level bases on the KEY column and name column NAME of table HX_L2.
+            """;
+
+    private static final String hierarchyBody = """
+            The Hierarchy1 is defined with the hasAll property set to false and the two levels H1_Level1 and H1_Level2.
+            """;
+
+    private static final String dimensionBody = """
+            The time dimension is defined with the one hierarchy.
+
+            """;
+
+    private static final String cubeBody = """
+            The cube with two DimensionConnectors.
+            One of them with level attribute to level H1_Level1
+            Second of them with level attribute to level H1_Level2
+            Level attribute in DimensionConnector uses for optimize sql inner join
+            Level attribute is name of the level to join to
+            If not specified joins to the lowest level of the dimension
+            """;
+
+    private static final String catalogDocumentationTxt = """
+    A basic OLAP schema with DimensionConnector with level attribute
+    Level attribute in DimensionConnector uses for optimize sql inner join
     Level attribute is name of the level to join to
     If not specified joins to the lowest level of the dimension
             """;
@@ -57,72 +122,72 @@ public class CatalogSupplier implements CatalogMappingSupplier {
 
         Column dimKeyColumn = RolapMappingFactory.eINSTANCE.createPhysicalColumn();
         dimKeyColumn.setName("DIM_KEY");
-        dimKeyColumn.setId("Fact_DIM_KEY");
+        dimKeyColumn.setId("_Fact_DIM_KEY");
         dimKeyColumn.setType(ColumnType.INTEGER);
 
         Column valueColumn = RolapMappingFactory.eINSTANCE.createPhysicalColumn();
         valueColumn.setName("VALUE");
-        valueColumn.setId("Fact_VALUE");
+        valueColumn.setId("_Fact_VALUE");
         valueColumn.setType(ColumnType.INTEGER);
 
         PhysicalTable table = RolapMappingFactory.eINSTANCE.createPhysicalTable();
         table.setName(FACT);
-        table.setId(FACT);
+        table.setId("_Fact");
         table.getColumns().addAll(List.of(dimKeyColumn, valueColumn));
         databaseSchema.getTables().add(table);
 
         Column h1L1KeyColumn = RolapMappingFactory.eINSTANCE.createPhysicalColumn();
         h1L1KeyColumn.setName("KEY");
-        h1L1KeyColumn.setId("H1_L1_KEY");
+        h1L1KeyColumn.setId("_H1_L1_KEY");
         h1L1KeyColumn.setType(ColumnType.INTEGER);
 
         Column h1L1NameColumn = RolapMappingFactory.eINSTANCE.createPhysicalColumn();
         h1L1NameColumn.setName("NAME");
-        h1L1NameColumn.setId("H1_L1_NAME");
+        h1L1NameColumn.setId("_H1_L1_NAME");
         h1L1NameColumn.setType(ColumnType.VARCHAR);
 
         PhysicalTable h1L1Table = RolapMappingFactory.eINSTANCE.createPhysicalTable();
         h1L1Table.setName("H1_L1");
-        h1L1Table.setId("H1_L1");
+        h1L1Table.setId("_H1_L1");
         h1L1Table.getColumns().addAll(List.of(h1L1KeyColumn, h1L1NameColumn));
         databaseSchema.getTables().add(h1L1Table);
 
         Column hxL2KeyColumn = RolapMappingFactory.eINSTANCE.createPhysicalColumn();
         hxL2KeyColumn.setName("KEY");
-        hxL2KeyColumn.setId("HX_L2_KEY");
+        hxL2KeyColumn.setId("_HX_L2_KEY");
         hxL2KeyColumn.setType(ColumnType.INTEGER);
 
         Column hxL2NameColumn = RolapMappingFactory.eINSTANCE.createPhysicalColumn();
         hxL2NameColumn.setName("NAME");
-        hxL2NameColumn.setId("HX_L2_NAME");
+        hxL2NameColumn.setId("_HX_L2_NAME");
         hxL2NameColumn.setType(ColumnType.VARCHAR);
 
         Column hxL2H1L1KeyColumn = RolapMappingFactory.eINSTANCE.createPhysicalColumn();
         hxL2H1L1KeyColumn.setName("H1L1_KEY");
-        hxL2H1L1KeyColumn.setId("HX_L2_H1L1_KEY");
+        hxL2H1L1KeyColumn.setId("_HX_L2_H1L1_KEY");
         hxL2H1L1KeyColumn.setType(ColumnType.INTEGER);
 
         Column hxL2H2L1KeyColumn = RolapMappingFactory.eINSTANCE.createPhysicalColumn();
         hxL2H2L1KeyColumn.setName("H2L1_KEY");
-        hxL2H2L1KeyColumn.setId("HX_L2_H2L1_KEY");
+        hxL2H2L1KeyColumn.setId("_HX_L2_H2L1_KEY");
         hxL2H2L1KeyColumn.setType(ColumnType.INTEGER);
 
         PhysicalTable hxL2Table = RolapMappingFactory.eINSTANCE.createPhysicalTable();
         hxL2Table.setName("HX_L2");
-        hxL2Table.setId("HX_L2");
+        hxL2Table.setId("_HX_L2");
         hxL2Table.getColumns().addAll(List.of(hxL2KeyColumn, hxL2NameColumn, hxL2H1L1KeyColumn, hxL2H2L1KeyColumn));
         databaseSchema.getTables().add(hxL2Table);
 
         TableQuery query = RolapMappingFactory.eINSTANCE.createTableQuery();
-        query.setId("FactQuery");
+        query.setId("_FactQuery");
         query.setTable(table);
 
         TableQuery queryH1L1 = RolapMappingFactory.eINSTANCE.createTableQuery();
-        queryH1L1.setId("H1L1Query");
+        queryH1L1.setId("_H1L1Query");
         queryH1L1.setTable(h1L1Table);
 
         TableQuery queryHxL2 = RolapMappingFactory.eINSTANCE.createTableQuery();
-        queryHxL2.setId("HxL2Query");
+        queryHxL2.setId("_HxL2Query");
         queryHxL2.setTable(hxL2Table);
 
         JoinedQueryElement left = RolapMappingFactory.eINSTANCE.createJoinedQueryElement();
@@ -134,7 +199,7 @@ public class CatalogSupplier implements CatalogMappingSupplier {
         right.setQuery(queryH1L1);
 
         JoinQuery joinQuery = RolapMappingFactory.eINSTANCE.createJoinQuery();
-        joinQuery.setId("joinQuery");
+        joinQuery.setId("_joinQuery");
         joinQuery.setLeft(left);
         joinQuery.setRight(right);
 
@@ -148,20 +213,20 @@ public class CatalogSupplier implements CatalogMappingSupplier {
 
         Level h1Level1 = RolapMappingFactory.eINSTANCE.createLevel();
         h1Level1.setName("H1_Level1");
-        h1Level1.setId("H1_Level1");
+        h1Level1.setId("_H1_Level1");
         h1Level1.setColumn(h1L1KeyColumn);
         h1Level1.setNameColumn(h1L1NameColumn);
 
         Level h1Level2 = RolapMappingFactory.eINSTANCE.createLevel();
         h1Level2.setName("H1_Level2");
-        h1Level2.setId("H1_Level2");
+        h1Level2.setId("_H1_Level2");
         h1Level2.setColumn(hxL2KeyColumn);
         h1Level2.setNameColumn(hxL2NameColumn);
 
         ExplicitHierarchy hierarchy1 = RolapMappingFactory.eINSTANCE.createExplicitHierarchy();
         hierarchy1.setHasAll(true);
         hierarchy1.setName("Hierarchy1");
-        hierarchy1.setId("Hierarchy1");
+        hierarchy1.setId("_Hierarchy1");
         hierarchy1.setPrimaryKey(hxL2KeyColumn);
         hierarchy1.setQuery(joinQuery);
         hierarchy1.getLevels().add(h1Level1);
@@ -169,7 +234,7 @@ public class CatalogSupplier implements CatalogMappingSupplier {
 
         StandardDimension dimension = RolapMappingFactory.eINSTANCE.createStandardDimension();
         dimension.setName("Diml1");
-        dimension.setId("Diml1");
+        dimension.setId("_Diml1");
         dimension.getHierarchies().add(hierarchy1);
 
         DimensionConnector dimensionConnector1 = RolapMappingFactory.eINSTANCE.createDimensionConnector();
@@ -188,20 +253,35 @@ public class CatalogSupplier implements CatalogMappingSupplier {
 
         PhysicalCube cube = RolapMappingFactory.eINSTANCE.createPhysicalCube();
         cube.setName(CUBE);
-        cube.setId(CUBE);
+        cube.setId("_Cube");
         cube.setQuery(query);
         cube.getMeasureGroups().add(measureGroup);
         cube.getDimensionConnectors().add(dimensionConnector1);
         cube.getDimensionConnectors().add(dimensionConnector2);
 
         Catalog catalog = RolapMappingFactory.eINSTANCE.createCatalog();
-        catalog.setName("Minimal_Cube_with_cube_dimension_level_attribute");
+        catalog.setName("Minimal Cube with cube with two dimension connectors with level attribute");
         catalog.setDescription("Schema of a minimal cube with level attribute");
         catalog.getCubes().add(cube);
         Documentation schemaDocumentation = RolapMappingFactory.eINSTANCE.createDocumentation();
-        schemaDocumentation.setValue(schemaDocumentationTxt);
+        schemaDocumentation.setValue(catalogDocumentationTxt);
         catalog.getDocumentations().add(schemaDocumentation);
         catalog.getDbschemas().add(databaseSchema);
+
+        document(catalog, "Catalog with Cube with two dimension connectors with level_attribute", catalogBody, 1, 0, 0, false, 0);
+        document(databaseSchema, "Database Schema", databaseSchemaBody, 1, 1, 0, true, 3);
+        document(query, "Query", queryBody, 1, 2, 0, true, 2);
+        document(queryH1L1, "H1L1Query", queryH1L1Body, 1, 2, 0, true, 2);
+        document(queryHxL2, "HxL2Query", queryHxL2Body, 1, 2, 0, true, 2);
+        document(joinQuery, "JoinQuery", joinQueryBody, 1, 2, 0, true, 2);
+        document(h1Level1, "H1_Level1", h1Level1Body, 1, 3, 0, true, 0);
+        document(h1Level2, "H1_Level2", h1Level2Body, 1, 4, 0, true, 0);
+
+        document(hierarchy1, "Hierarchy1", hierarchyBody, 1, 8, 0, true, 0);
+        document(dimension, "Diml1", dimensionBody, 1, 9, 0, true, 0);
+
+        document(cube, "Cube", cubeBody, 1, 10, 0, true, 2);
+
         return catalog;
     }
 
