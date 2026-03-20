826b32f3e253e2add40c2af0b13c62cad8604476
dbulahov <bulahovdenis@gmail.com>
2026-03-20 12:08:08 +0300

    Levels with ordinal columns tutorial Signed-off-by: dbulahov <bulahovdenis@gmail.com>

---
+++ instance/emf/tutorial/level/ordinalcolumns/src/main/java/org/eclipse/daanse/rolap/mapping/instance/emf/tutorial/level/ordinalcolumns/CatalogSupplier.java
@@ -0,0 +1,198 @@
+/*
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *
+ */
+package org.eclipse.daanse.rolap.mapping.instance.emf.tutorial.level.ordinalcolumns;
+
+import static org.eclipse.daanse.rolap.mapping.model.provider.util.DocumentationUtil.document;
+
+import java.util.List;
+
+import org.eclipse.daanse.rolap.mapping.model.provider.CatalogMappingSupplier;
+import org.eclipse.daanse.rolap.mapping.instance.api.Kind;
+import org.eclipse.daanse.rolap.mapping.instance.api.MappingInstance;
+import org.eclipse.daanse.rolap.mapping.instance.api.Source;
+import org.eclipse.daanse.rolap.mapping.model.Catalog;
+import org.eclipse.daanse.rolap.mapping.model.Column;
+import org.eclipse.daanse.rolap.mapping.model.ColumnType;
+import org.eclipse.daanse.rolap.mapping.model.DatabaseSchema;
+import org.eclipse.daanse.rolap.mapping.model.DimensionConnector;
+import org.eclipse.daanse.rolap.mapping.model.ExplicitHierarchy;
+import org.eclipse.daanse.rolap.mapping.model.Level;
+import org.eclipse.daanse.rolap.mapping.model.MeasureGroup;
+import org.eclipse.daanse.rolap.mapping.model.OrderedColumn;
+import org.eclipse.daanse.rolap.mapping.model.PhysicalCube;
+import org.eclipse.daanse.rolap.mapping.model.PhysicalTable;
+import org.eclipse.daanse.rolap.mapping.model.RolapMappingFactory;
+import org.eclipse.daanse.rolap.mapping.model.SortingDirection;
+import org.eclipse.daanse.rolap.mapping.model.StandardDimension;
+import org.eclipse.daanse.rolap.mapping.model.SumMeasure;
+import org.eclipse.daanse.rolap.mapping.model.TableQuery;
+import org.osgi.service.component.annotations.Component;
+
+@MappingInstance(kind = Kind.TUTORIAL, number = "2.14.06", source = Source.EMF, group = "Level") // NOSONAR
+@Component(service = CatalogMappingSupplier.class)
+public class CatalogSupplier implements CatalogMappingSupplier {
+
+    private static final String introBody = """
+            This Tutorial shows how to use Ordinal columns in levels. This Tutorial shows `Country` with sorting ascending and `Town` with sorting descending
+            Ordinal columns parameter can to use with multiple columns. in this case, sorting occurs by several columns
+            OrderedColumn is typically used in OLAP contexts where explicit column ordering is required for query processing or result presentation.
+            """;
+
+    private static final String databaseSchemaBody = """
+            The cube defined in this example is based on one table. The `Fact` table contains a columns the name of the `Town` and the `Country`.
+            """;
+
+    private static final String levelTownBody = """
+            The level of the `Town` used the `column` attribute to define the column that holds the name, which is also the key Column.
+            """;
+
+    private static final String levelCountryBody = """
+            The level  of the `Country` used the `column` attribute to define the column that holds the name, which is also the key Column.
+            """;
+
+    private static final String orderedColumnCountryBody = """
+            OrderedColumn for level country. OrderedColumn use ascending sorting by country name
+            """;
+
+    private static final String orderedColumnTownBody = """
+            OrderedColumn for level town. OrderedColumn use ascending sorting by town name
+            """;
+
+    private static final String hierarchyBody = """
+            This Hierarchy contains both defined levels. The `primaryKey` attribute defines the column that contains the primary key of the hierarchy. The `query` attribute references to the query that will be used to retrieve the data for the hierarchy.
+
+            The order of the Levels in the hierarchy is important, as it determines the drill-down path for the hierarchy.
+            """;
+
+    private static final String dimensionBody = """
+            The Dimension has only one hierarchy.
+            """;
+
+    private static final String cubeBody = """
+            The cube contains only one Measure in a unnamed MeasureGroup and references to the Dimension.
+
+            To connect the dimension to the cube, a DimensionConnector is used.
+            """;
+
+    private static final String queryFactBody = """
+            The TableQuery for the Levels and the Measure.
+            """;
+
+    @Override
+    public Catalog get() {
+        DatabaseSchema databaseSchema = RolapMappingFactory.eINSTANCE.createDatabaseSchema();
+        databaseSchema.setId("_databaseSchema_main");
+
+        Column columnKey = RolapMappingFactory.eINSTANCE.createPhysicalColumn();
+        columnKey.setName("KEY");
+        columnKey.setId("_column_fact_key");
+        columnKey.setType(ColumnType.VARCHAR);
+
+        Column columnValue = RolapMappingFactory.eINSTANCE.createPhysicalColumn();
+        columnValue.setName("VALUE");
+        columnValue.setId("_column_fact_value");
+        columnValue.setType(ColumnType.INTEGER);
+
+        Column columnCountry = RolapMappingFactory.eINSTANCE.createPhysicalColumn();
+        columnCountry.setName("COUNTRY");
+        columnCountry.setId("_column_fact_country");
+        columnCountry.setType(ColumnType.VARCHAR);
+
+        PhysicalTable tableFact = RolapMappingFactory.eINSTANCE.createPhysicalTable();
+        tableFact.setName("Fact");
+        tableFact.setId("_table_fact");
+        tableFact.getColumns().addAll(List.of(columnKey,columnCountry, columnValue));
+        databaseSchema.getTables().add(tableFact);
+
+        TableQuery query = RolapMappingFactory.eINSTANCE.createTableQuery();
+        query.setId("_query_fact");
+        query.setTable(tableFact);
+
+        SumMeasure measure = RolapMappingFactory.eINSTANCE.createSumMeasure();
+        measure.setName("theMeasure");
+        measure.setId("_measure_theMeasure");
+        measure.setColumn(columnValue);
+
+        MeasureGroup measureGroup = RolapMappingFactory.eINSTANCE.createMeasureGroup();
+        measureGroup.getMeasures().add(measure);
+
+        OrderedColumn countryOrderedColumn = RolapMappingFactory.eINSTANCE.createOrderedColumn();
+        countryOrderedColumn.setColumn(columnCountry);
+        countryOrderedColumn.setId("_ordered_column_country");
+        countryOrderedColumn.setDirection(SortingDirection.ASC);
+
+        OrderedColumn townOrderedColumn = RolapMappingFactory.eINSTANCE.createOrderedColumn();
+        townOrderedColumn.setColumn(columnKey);
+        townOrderedColumn.setId("_ordered_column_town");
+        townOrderedColumn.setDirection(SortingDirection.DESC);
+
+        Level levelTown = RolapMappingFactory.eINSTANCE.createLevel();
+        levelTown.setName("Town");
+        levelTown.setId("_level_town");
+        levelTown.setColumn(columnKey);
+        levelTown.getOrdinalColumns().add(townOrderedColumn);
+
+        Level levelCountry = RolapMappingFactory.eINSTANCE.createLevel();
+        levelCountry.setName("Country");
+        levelCountry.setId("_level_country");
+        levelCountry.setColumn(columnCountry);
+        levelCountry.getOrdinalColumns().add(countryOrderedColumn);
+
+        ExplicitHierarchy hierarchy = RolapMappingFactory.eINSTANCE.createExplicitHierarchy();
+        hierarchy.setName("CountryHierarchy");
+        hierarchy.setId("_hierarchy_countryHierarchy");
+        hierarchy.setPrimaryKey(columnKey);
+        hierarchy.setQuery(query);
+        hierarchy.getLevels().add(levelCountry);
+        hierarchy.getLevels().add(levelTown);
+
+        StandardDimension dimension = RolapMappingFactory.eINSTANCE.createStandardDimension();
+        dimension.setName("Country");
+        dimension.setId("_dimension_country");
+        dimension.getHierarchies().add(hierarchy);
+
+        DimensionConnector dimensionConnector1 = RolapMappingFactory.eINSTANCE.createDimensionConnector();
+        dimensionConnector1.setId("_dimensionConnector_country");
+        dimensionConnector1.setDimension(dimension);
+        dimensionConnector1.setForeignKey(columnCountry);
+
+        PhysicalCube cube = RolapMappingFactory.eINSTANCE.createPhysicalCube();
+        cube.setName("Cube with ordinal columns");
+        cube.setId("_cube_with_ordinal_columns");
+        cube.setQuery(query);
+        cube.getMeasureGroups().add(measureGroup);
+        cube.getDimensionConnectors().add(dimensionConnector1);
+
+        Catalog catalog = RolapMappingFactory.eINSTANCE.createCatalog();
+        catalog.getDbschemas().add(databaseSchema);
+        catalog.setName("Daanse Tutorial - Levels with ordinal columns");
+        catalog.setDescription("Levels with ordinal columns");
+        catalog.getCubes().add(cube);
+
+        document(catalog, "Daanse Tutorial - Levels with ordinal columns", introBody, 1, 0, 0, false, 0);
+        document(databaseSchema, "Database Schema", databaseSchemaBody, 1, 1, 0, true, 3);
+        document(query, "Query Fact", queryFactBody, 1, 3, 0, true, 2);
+
+        document(levelTown, "Level town", levelTownBody, 1, 4, 0, true, 0);
+        document(levelCountry, "Level country", levelCountryBody, 1, 5, 0, true, 0);
+        document(countryOrderedColumn, "Ordered Column Country", orderedColumnCountryBody, 1, 6, 0, true, 0);
+        document(countryOrderedColumn, "Ordered Column Country", orderedColumnTownBody, 1, 7, 0, true, 0);
+        document(hierarchy, "Hierarchy", hierarchyBody, 1, 8, 0, true, 0);
+        document(dimension, "Dimension", dimensionBody, 1, 9, 0, true, 0);
+
+        document(cube, "Cube and DimensionConnector and Measure", cubeBody, 1, 10, 0, true, 2);
+
+        return catalog;
+    }
+
+}
